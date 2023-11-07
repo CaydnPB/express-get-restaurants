fd4a77fa699f69b62363004bc56c9a3b14bbc081
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-07 10:47:57 +0000

    Add wildcard parameter to GET request

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll();
   res.json(restaurants);
 });
+
+app.get("/restaurants/:id", async (req, res) => {
+  const rest = await Restaurant.findByPk(req.params.id);
+  res.json(rest);
+})
 
 module.exports = app;
