e8821a47de9b8852bd54b133e5ec76808d9c47ce
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-07 11:26:45 +0000

    Add CRUD operations to database connection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll();
@@ -13,6 +14,24 @@ app.get("/restaurants", async (req, res) => {
 app.get("/restaurants/:id", async (req, res) => {
   const rest = await Restaurant.findByPk(req.params.id);
   res.json(rest);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const rest = await Restaurant.create(req.body);
+  const restaurants = await Restaurant.findAll();
+  res.json(restaurants);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const updatedRest = await Restaurant.update(req.body, {where: {id: req.params.id}});
+  const restaurants = await Restaurant.findAll();
+  res.json(restaurants);
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const deletedRest = await Restaurant.destroy({where: {id: req.params.id}});
+  const restaurants = await Restaurant.findAll();
+  res.json(restaurants);
 })
 
 module.exports = app;
